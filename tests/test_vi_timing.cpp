509bcabaa0794aff61714b728e6ea46f431549d3
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-08-21 19:10:47 +0300

    step

--- tests/test_vi_timing.cpp
+++ tests/test_vi_timing.cpp
@@ -38,24 +38,64 @@ namespace
 			"\tVersion: " << VI_TM_FULLVERSION << "\n"
 			"\tGit describe: " << static_cast<const char *>(vi_tmStaticInfo(VI_TM_INFO_GIT_DESCRIBE)) << "\n"
 			"\tGit commit date and time: " << static_cast<const char *>(vi_tmStaticInfo(VI_TM_INFO_GIT_DATETIME)) << "\n";
-		endl(std::cout);
+		endl(stream);
 
 		const auto unit = *static_cast<const double *>(vi_tmStaticInfo(VI_TM_INFO_UNIT));
 		const auto duration = *static_cast<const double *>(vi_tmStaticInfo(VI_TM_INFO_DURATION));
 		const auto overhead = *static_cast<const double *>(vi_tmStaticInfo(VI_TM_INFO_OVERHEAD));
 		const auto resolution = *static_cast<const double *>(vi_tmStaticInfo(VI_TM_INFO_RESOLUTION));
 		stream <<
-			"\tResolution: " << static_cast<int>(resolution * unit * 1e9) << " ns.\n"
-			"\tDuration: " << static_cast<int>(duration * unit * 1e9) << " ns.\n";
-		endl(std::cout);
+			"Static info:\n"
+			"\tDivision price: " << static_cast<int>(unit * 1e12) << " ps.\n"
+			"\tClock resolution: " << static_cast<int>(resolution * unit * 1e9) << " ns.\n"
+			"\tMeasurement duration: " << static_cast<int>(duration * unit * 1e9) << " ns.\n";
+		endl(stream);
+	}
+
+	void metrics(std::ostream &stream)
+	{	vi_CurrentThreadAffinityFixate();
+
+		stream << "Calculated:\n";
+		{	constexpr auto N = 8U;
+			std::this_thread::yield(); // Reduce likelihood of thread interruption during measurement.
+			const auto first = vi_tmGetTicks();
+			auto last = first;
+			for (auto cnt = N; cnt; )
+			{	if (const auto current = vi_tmGetTicks(); current != last)
+				{	last = current;
+					--cnt;
+				}
+			}
+			stream << "\tvi_tmGetTicks() resolution: " << (last - first) / N << " ticks.\n";
+		}
+
+		{	constexpr auto CNT = 1'000'000U;
+			volatile auto _ = vi_tmGetTicks();
+			vi_ThreadYield();
+			const auto start = ch::steady_clock::now();
+			for (unsigned n = 0U; n < CNT; ++n)
+			{
+				_ = vi_tmGetTicks();
+			}
+			const auto finish = ch::steady_clock::now();
+			const auto duration = ch::duration_cast<ch::nanoseconds>(finish - start).count();
+			stream << "\tvi_tmGetTicks() duration: " << duration / CNT << " ns.\n";
+		}
+
+		vi_CurrentThreadAffinityRestore();
 	}
 }
 
 int main(int argc, char** argv)
 {
-	header(std::cout);
-
 	const bool gtest_list_tests = std::any_of(argv, argv + argc, [](auto arg) { return "--gtest_list_tests"sv == arg; });
+
+	if (!gtest_list_tests)
+	{	header(std::cout);
+		vi_WarmUp();
+		metrics(std::cout);
+		endl(std::cout);
+	}
 
     ::testing::InitGoogleTest(&argc, argv);
 	::benchmark::Initialize(&argc, argv);
@@ -69,9 +109,7 @@ int main(int argc, char** argv)
 	}
 
 	if (!gtest_list_tests)
-	{
-		vi_WarmUp(1U);
-		endl(std::cout);
+	{	endl(std::cout);
 		std::cout << "Benchmark:\n";
 		::benchmark::RunSpecifiedBenchmarks();
 	}
