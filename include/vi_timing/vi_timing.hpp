e39026517780342d827d311290add2ab37a9f695
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-08-22 12:00:53 +0300

    step

--- include/vi_timing/vi_timing.hpp
+++ include/vi_timing/vi_timing.hpp
@@ -165,7 +165,7 @@ namespace vi_tm
 
 	inline std::string to_string(double val, unsigned char sig = 2U, unsigned char dec = 1U)
 	{	std::string result;
-		result.resize(sig + (9 + 1 + 1), '\0');
+		result.resize(sig + (9 + 1 + 1), '\0'); // "-00S.Se-308"
 		const auto len = vi_tmF2A(result.data(), static_cast<unsigned char>(result.size()), val, sig, dec);
 		result.resize(len - 1);
 		return result;
