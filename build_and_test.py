73118e9e37b8ad5022efd1b39ed83add5d66611a
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-09-01 17:47:39 +0300

    bach tests

--- build_and_test.py
+++ build_and_test.py
@@ -44,29 +44,34 @@ def get_cmake_property(key: str) -> str | None:
 	return None
 
 def remove_readonly(func, path, _):
-    os.chmod(path, stat.S_IWRITE)
-    func(path)
+	os.chmod(path, stat.S_IWRITE)
+	func(path)
 
 def run(cmd, cwd=None):
 	print(f"RUN: {' '.join(cmd)} (cwd={cwd or os.getcwd()})")
 	result = subprocess.run(cmd, cwd=cwd, check=True)
 
-def parse_params()->argparse.Namespace:
-	project_root = os.path.dirname(__file__) if '__file__' in globals() else os.getcwd()
-	test_root = os.path.join(project_root, "_test")
+PROJECT_ROOT = os.path.dirname(__file__) if '__file__' in globals() else os.getcwd()
 
+def parse_params()->argparse.Namespace:
 	parser = argparse.ArgumentParser(description="Automate testing for all CMake option combinations")
 	parser.add_argument(
 		"-S",
 		"--path-to-source",
-		default=project_root,
+		default=PROJECT_ROOT,
 		help="Explicitly specify a source directory."
 	)
 	parser.add_argument(
 		"-B",
 		"--path-to-build",
-		default=test_root,
+		default=PROJECT_ROOT,
 		help="Explicitly specify a build directory."
+	)
+	parser.add_argument(
+		"-T",
+		"--path-to-result",
+		default=os.path.join(PROJECT_ROOT, "bin"),
+		help="Explicitly specify a target directory."
 	)
 	parser.add_argument(
 		"params",
@@ -94,11 +99,9 @@ def get_pair(combo)-> tuple[str, list[str]]:
 
 	return suffix, params
 
-
 def folder_prepare(path: str):
-	if os.path.exists(path) and os.path.isdir(path):
-		shutil.rmtree(path, onerror=remove_readonly)
-	os.makedirs(path)
+	if not os.path.exists(path):
+		os.makedirs(path)
 
 def filter_suffix(suffix: str, suffix_filters: str)->bool:
 	return suffix_filters and suffix not in suffix_filters
@@ -108,30 +111,42 @@ def main():
 	print(f"[START ALL]: {start_all.strftime("%H:%M:%S")}")
 
 	PARAMS = parse_params()
-
+	
 	path_to_source = PARAMS.path_to_source
+	if not os.path.isabs(path_to_source):
+		path_to_source = os.path.abspath(os.path.join(PROJECT_ROOT, path_to_source))
+
 	test_root = PARAMS.path_to_build
+	if not os.path.isabs(test_root):
+		test_root = os.path.abspath(os.path.join(PROJECT_ROOT, test_root))
+
+	path_to_result = PARAMS.path_to_result
+	if not os.path.isabs(path_to_result):
+		path_to_result = os.path.abspath(os.path.join(PROJECT_ROOT, path_to_result))
+
 	suffix_filters = PARAMS.params
 
 	folder_prepare(test_root)
 
 	for combo in itertools.product([False, True], repeat=len(OPTIONS)):
-		suffix, params = get_pair(combo)
+		suffix, options = get_pair(combo)
 		if filter_suffix(suffix, suffix_filters):
 			continue
 
-		params += ["-DCMAKE_BUILD_TYPE=Release"]
 		name = "vi_timing_" + suffix
 
 		start = datetime.datetime.now()
 		print(f"[START] {name}: {start.strftime("%H:%M:%S")}")
 
-		build_dir = os.path.join(test_root, name)
-		os.makedirs(build_dir)
+		build_dir = os.path.join(test_root, "_build_" + suffix)
+		if os.path.exists(build_dir) and os.path.isdir(build_dir):
+			shutil.rmtree(build_dir, onerror=remove_readonly)
+#		os.makedirs(build_dir)
 
 		print("Configuring CMake:")
-		cmake_args = ["cmake", "-S", path_to_source, "-B", build_dir] + params
-		run(cmake_args)
+		params = ["cmake", "-S", path_to_source, "-B", build_dir, "-DCMAKE_BUILD_TYPE=Release", f"-DVI_TM_OUTPUT_PATH={path_to_result}"]
+		params += options
+		run(params)
 		print("Configuring CMake - done\n")
 
 		print("Build the project:")
