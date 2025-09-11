57db766d03e0bf3e68442f3a1c45a94b55e4c373
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-09-11 10:09:52 +0300

    Linux fix

--- tests/test_lib/header.h
+++ tests/test_lib/header.h
@@ -2,10 +2,21 @@
 #	define VI2_TESTS_LIB_HEADER_H 0.1.0
 #	pragma once
 
-#ifdef VI_TM_TEST_LIB_EXPORTS
+
+#if defined(_MSC_VER)
+#	if defined(VI_TM_TEST_LIB_EXPORTS)
 #		define VI_TM_TEST_LIB_API __declspec(dllexport)
-#else
+#	else
 #		define VI_TM_TEST_LIB_API __declspec(dllimport)
+#	endif
+#elif defined (__GNUC__) || defined(__clang__)
+#	ifdef VI_TM_TEST_LIB_EXPORTS
+#		define VI_TM_TEST_LIB_API __attribute__((visibility("default")))
+#	else
+#		define VI_TM_TEST_LIB_API
+#	endif
+#else
+#	define VI_TM_TEST_LIB_API
 #endif
 
 extern "C" VI_TM_TEST_LIB_API void test_lib_func(void);
