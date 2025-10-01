1c532a956e50ebf87312f966a92831e998361b01
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-10-01 13:39:49 +0300

    Redundant tests have been removed.

--- tests/test_exe/benchmark.cpp
+++ tests/test_exe/benchmark.cpp
@@ -55,15 +55,6 @@ BENCHMARK(BM_probe);
 static void BM_probe_ext(benchmark::State &state)
 {	VI_TM_RESET("xxxx");
 	auto m = vi_tmJournalGetMeas(VI_TM_HGLOBAL, "xxxx");
-	for (auto _ : state)
-	{	vi_tm::probe_t probe{ m };
-	}
-}
-BENCHMARK(BM_probe_ext);
-
-static void BM_probe_ext_pr(benchmark::State &state)
-{	VI_TM_RESET("xxxx");
-	auto m = vi_tmJournalGetMeas(VI_TM_HGLOBAL, "xxxx");
 	vi_tm::probe_t probe{ std::true_type{}, m};
 	for (auto _ : state)
 	{	
@@ -72,7 +63,7 @@ static void BM_probe_ext_pr(benchmark::State &state)
 	}
 	probe.stop();
 }
-BENCHMARK(BM_probe_ext_pr);
+BENCHMARK(BM_probe_ext);
 
 static void BM_vi_tm(benchmark::State& state) {
 	VI_TM_RESET("xxxx");
