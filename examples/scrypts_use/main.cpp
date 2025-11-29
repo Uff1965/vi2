cfcb94298929288a5f42370ab3a04598e9387a73
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-11-29 15:07:41 +0300

    CMakeLists.txt debug info in release fix

--- examples/scrypts_use/main.cpp
+++ examples/scrypts_use/main.cpp
@@ -6,8 +6,6 @@
 
 VI_TM(FILE_PATH);
 
-VI_TM_HREG h_register = VI_TM_HGLOBAL;
-
 namespace
 {
 	std::map<std::string, test_func_t>& instance()
@@ -15,6 +13,8 @@ namespace
 		return instance;
 	}
 }
+
+VI_TM_HREG h_register = VI_TM_HGLOBAL;
 
 int register_test(const char* name, test_func_t fn)
 {	instance().try_emplace(name, fn);
@@ -26,14 +26,19 @@ int main()
 
 	auto hreg = vi_tmRegistryCreate();
 
-	{	VI_TM_H(hreg, "1");
-		VI_TM_GLOBALINIT("Timing report:\n", vi_tmSortByName, vi_tmSortAscending, "Goodbye!\n");
+	VI_TM_GLOBALINIT(
+		vi_tmSortByName, vi_tmSortAscending, vi_tmShowResolution, vi_tmShowDuration,
+		"Timing report:\n",
+		"Goodbye!\n"
+	);
 
-		std::fprintf(stdout, "Hellow, World!\n\n");
+	std::fprintf(stdout, "Hellow, World!\n\n");
 
-		vi_CurrentThreadAffinityFixate();
-		vi_WarmUp(1, 500);
-	}
+	vi_CurrentThreadAffinityFixate();
+//		vi_WarmUp(1, 500);
+	auto secperunit = *static_cast<const double*>(vi_tmStaticInfo(vi_tmInfoSecPerUnit));
+	auto overhead = secperunit * *static_cast<const double*>(vi_tmStaticInfo(vi_tmInfoOverhead));
+	fprintf(stdout, "Overhead: %.0f ns.\n\n", 1e9 * overhead);
 
 	{	std::fprintf(stdout, "First execution:\n");
 		h_register = vi_tmRegistryCreate();
@@ -44,8 +49,7 @@ int main()
 				std::fprintf(stdout, "Test: \'%s\' ...", name.c_str());
 				{	auto resume = tm.scoped_resume();
 					if (!func())
-					{
-						assert(false);
+					{	assert(false);
 						std::fprintf(stderr, "Test %s failed\n", name.c_str());
 						return 1;
 					}
@@ -53,16 +57,10 @@ int main()
 				std::fprintf(stdout, "done\n");
 			}
 		}
-		{	VI_TM_H(hreg, "2");
-			vi_tmRegistryReport(h_register, vi_tmSortByName | vi_tmSortAscending);
-		}
-		{	VI_TM_H(hreg, "3");
-			std::fprintf(stdout, "\n");
-		}
-		{	VI_TM_H(hreg, "4");
-			vi_tmRegistryClose(h_register);
-			h_register = VI_TM_HGLOBAL;
-		}
+		vi_tmRegistryReport(h_register, vi_tmSortByName | vi_tmSortAscending);
+		std::fprintf(stdout, "\n");
+		vi_tmRegistryClose(h_register);
+		h_register = VI_TM_HGLOBAL;
 	}
 
 	std::fprintf(stdout, "Other executions:\nTesting: ");
@@ -71,8 +69,7 @@ int main()
 		auto tm = vi_tm::scoped_probe_t::make_paused(h_meas);
 		auto len = std::fprintf(stdout, "%02d/100 ... ", n + 1);
 		for (const auto &[name, func] : instance())
-		{
-			vi_ThreadYield();
+		{	vi_ThreadYield();
 			std::fflush(stdout);
 			{	auto resume = tm.scoped_resume();
 				if (!func())
@@ -87,15 +84,9 @@ int main()
 		}
 	}
 
-	{	VI_TM_H(hreg, "5");
-		std::fprintf(stdout, "All tests done.\n\n");
-	}
-	{	VI_TM_H(hreg, "6");
-		vi_CurrentThreadAffinityRestore();
-	}
-	{	VI_TM_H(hreg, "7");
-		std::fprintf(stdout, "\nMisc:");
-	}
+	std::fprintf(stdout, "All tests done.\n\n");
+	vi_CurrentThreadAffinityRestore();
+	std::fprintf(stdout, "\nMisc:\n");
 
 	vi_tmRegistryReport(hreg, vi_tmSortByTime);
 	vi_tmRegistryClose(hreg);
